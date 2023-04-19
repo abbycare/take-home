be9f8704835f821cdce68347d8906f983db6d759
Vijay Raju <vijaypraju04@gmail.com>
2023-04-19 13:19:45 -0700

    add scaffold for take home

---
+++ wellspring-take-home/server/recentPatients.js
@@ -0,0 +1,24 @@
+const recentPatients = [
+    { id: 1, patientName: 'John Doe', caregiverName: 'Jane Doe', age: 30, visitStatus: 'Checked In' },
+    { id: 2, patientName: 'Bob Smith', caregiverName: 'Sarah Smith', age: 25, visitStatus: 'Not Checked In' },
+    { id: 3, patientName: 'Alice Lee', caregiverName: 'Mark Lee', age: 45, visitStatus: 'Not Checked In' },
+    { id: 4, patientName: 'David Johnson', caregiverName: 'Maria Johnson', age: 55, visitStatus: 'Checked In' },
+    { id: 5, patientName: 'Peter Davis', caregiverName: 'Mary Davis', age: 40, visitStatus: 'Not Checked In' },
+    { id: 6, patientName: 'Kevin Brown', caregiverName: 'Melissa Brown', age: 35, visitStatus: 'Checked In' },
+    { id: 7, patientName: 'Michael Garcia', caregiverName: 'Susan Garcia', age: 50, visitStatus: 'Not Checked In' },
+    { id: 8, patientName: 'William Rodriguez', caregiverName: 'Jennifer Rodriguez', age: 28, visitStatus: 'Checked In' },
+    { id: 9, patientName: 'Linda Wilson', caregiverName: 'Richard Wilson', age: 60, visitStatus: 'Not Checked In' },
+    { id: 10, patientName: 'Karen Jackson', caregiverName: 'Jason Jackson', age: 32, visitStatus: 'Checked In' },
+    { id: 11, patientName: 'Steven Miller', caregiverName: 'Emily Miller', age: 38, visitStatus: 'Not Checked In' },
+    { id: 12, patientName: 'Thomas Taylor', caregiverName: 'Laura Taylor', age: 42, visitStatus: 'Checked In' },
+    { id: 13, patientName: 'George Moore', caregiverName: 'Caroline Moore', age: 33, visitStatus: 'Not Checked In' },
+    { id: 14, patientName: 'Andrew Clark', caregiverName: 'Julia Clark', age: 47, visitStatus: 'Checked In' },
+    { id: 15, patientName: 'Timothy Baker', caregiverName: 'Christina Baker', age: 27, visitStatus: 'Not Checked In' },
+    { id: 16, patientName: 'Edward Perez', caregiverName: 'Margaret Perez', age: 53, visitStatus: 'Checked In' },
+    { id: 17, patientName: 'Amanda Evans', caregiverName: 'Nicholas Evans', age: 44, visitStatus: 'Not Checked In' },
+    { id: 18, patientName: 'Olivia Hernandez', caregiverName: 'Matthew Hernandez', age: 31, visitStatus: 'Checked In' },
+    { id: 19, patientName: 'Dorothy Sanchez', caregiverName: 'Henry Sanchez', age: 58, visitStatus: 'Not Checked In' },
+    { id: 20, patientName: 'Franklin Turner', caregiverName: 'Hannah Turner', age: 39, visitStatus: 'Checked In' }
+  ];
+  
+  module.exports = recentPatients;
---
+++ wellspring-take-home/server/upcomingVisits.js
@@ -0,0 +1,31 @@
+const upcomingVisits = [
+    {
+      id: 1,
+      time: "10:00 AM",
+      appointmentType: "Telehealth",
+      patientName: "John Smith",
+      appointmentName: "PAT",
+      appointmentDate: "2023-04-24",
+      appointmentDescription: "Annual checkup"
+    },
+    {
+      id: 2,
+      time: "2:00 PM",
+      appointmentType: "Telehealth",
+      patientName: "Robert Johnson",
+      appointmentName: "PAT",
+      appointmentDate: "2023-04-25",
+      appointmentDescription: "Discuss treatment options"
+    },
+    {
+      id: 3,
+      time: "11:00 AM",
+      appointmentType: "In-person",
+      patientName: "Mary Smith",
+      appointmentName: "Delegation",
+      appointmentDate: "2023-04-26",
+      appointmentDescription: "Annual checkup"
+    }
+  ];
+
+  module.exports = upcomingVisits;
