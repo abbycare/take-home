da6ea8c93a0fb46c0cc58fd72530928a478acae0
Vijay Raju <vijaypraju04@gmail.com>
2023-04-19 17:54:49 -0700

    add dynamic dates and fix api names

--- wellspring-take-home/server/appointments.js
+++ wellspring-take-home/server/appointments.js
@@ -1,31 +1,40 @@
-const upcomingVisits = [
+const appointments = [
     {
       id: 1,
       time: "10:00 AM",
       appointmentType: "Telehealth",
       patientName: "John Smith",
       appointmentName: "PAT",
-      appointmentDate: "2023-04-24",
+      appointmentDate: new Date().toLocaleDateString(),
       appointmentDescription: "Annual checkup"
     },
     {
       id: 2,
+      time: "4:00 PM",
+      appointmentType: "Telehealth",
+      patientName: "George Mason",
+      appointmentName: "PAT",
+      appointmentDate: new Date().toLocaleDateString(),
+      appointmentDescription: "Annual checkup"
+    },
+    {
+      id: 3,
       time: "2:00 PM",
       appointmentType: "Telehealth",
       patientName: "Robert Johnson",
       appointmentName: "PAT",
-      appointmentDate: "2023-04-25",
+      appointmentDate: new Date(Date.now() + 24 * 60 * 60 * 1000).toLocaleDateString(),
       appointmentDescription: "Discuss treatment options"
     },
     {
-      id: 3,
+      id: 4,
       time: "11:00 AM",
       appointmentType: "In-person",
       patientName: "Mary Smith",
       appointmentName: "Delegation",
-      appointmentDate: "2023-04-26",
+      appointmentDate: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000).toLocaleDateString(),
       appointmentDescription: "Annual checkup"
     }
   ];
 
-  module.exports = upcomingVisits;
+  module.exports = appointments;
---
+++ wellspring-take-home/server/patients.js
@@ -0,0 +1,24 @@
+const patients = [
+  { id: 1, patientName: 'John Doe', caregiverName: 'Jane Doe', visitStatus: 'Visit completed', lastCheckIn: '2023-04-19' },
+  { id: 2, patientName: 'Bob Smith', caregiverName: 'Sarah Smith', visitStatus: 'Started training', lastCheckIn: '2023-04-18' },
+  { id: 3, patientName: 'Alice Lee', caregiverName: 'Mark Lee', visitStatus: 'Visit completed', lastCheckIn: '2023-04-08' },
+  { id: 4, patientName: 'David Johnson', caregiverName: 'Maria Johnson', visitStatus: 'Started training', lastCheckIn: '2023-04-08' },
+  { id: 5, patientName: 'Peter Davis', caregiverName: 'Mary Davis', visitStatus: 'Missing documentation', lastCheckIn: '2023-04-05' },
+  { id: 6, patientName: 'Kevin Brown', caregiverName: 'Melissa Brown', visitStatus: 'Visit completed', lastCheckIn: '2023-04-04' },
+  { id: 7, patientName: 'Michael Garcia', caregiverName: 'Susan Garcia', visitStatus: 'Started training', lastCheckIn: '2023-04-14' },
+  { id: 8, patientName: 'William Rodriguez', caregiverName: 'Jennifer Rodriguez', visitStatus: 'Visit completed', lastCheckIn: '2023-04-12' },
+  { id: 9, patientName: 'Linda Wilson', caregiverName: 'Richard Wilson', visitStatus: 'Visit completed', lastCheckIn: '2023-04-13' },
+  { id: 10, patientName: 'Karen Jackson', caregiverName: 'Jason Jackson', visitStatus: 'Started training', lastCheckIn: '2023-04-10' },
+  { id: 11, patientName: 'Steven Miller', caregiverName: 'Emily Miller', visitStatus: 'Missing documentation', lastCheckIn: '2023-04-09' },
+  { id: 12, patientName: 'Thomas Taylor', caregiverName: 'Laura Taylor', visitStatus: 'Started training', lastCheckIn: '2023-04-08' },
+  { id: 13, patientName: 'George Moore', caregiverName: 'Caroline Moore', visitStatus: 'Visit completed', lastCheckIn: '2023-04-07' },
+  { id: 14, patientName: 'Andrew Clark', caregiverName: 'Julia Clark', visitStatus: 'Missing documentation', lastCheckIn: '2023-04-06' },
+  { id: 15, patientName: 'Timothy Baker', caregiverName: 'Christina Baker', visitStatus: 'Visit completed', lastCheckIn: '2023-04-05' },
+  { id: 16, patientName: 'Edward Perez', caregiverName: 'Margaret Perez', visitStatus: 'Missing documentation', lastCheckIn: '2023-04-17' },
+  { id: 17, patientName: 'Amanda Evans', caregiverName: 'Nicholas Evans', visitStatus: 'Visit completed', lastCheckIn: '2023-04-15' },
+  { id: 18, patientName: 'Olivia Hernandez', caregiverName: 'Matthew Hernandez', visitStatus: 'Started training', lastCheckIn: '2023-04-14' },
+  { id: 19, patientName: 'Dorothy Sanchez', caregiverName: 'Henry Sanchez', visitStatus: 'Visit completed', lastCheckIn: '2023-04-12' },
+  { id: 20, patientName: 'Franklin Turner', caregiverName: 'Hannah Turner', visitStatus: 'Missing documentation', lastCheckIn: '2023-04-11' }
+]
+
+module.exports = patients;
